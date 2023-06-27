f8ea2885bed9c4ccba270e8aa08ed268f16f3c64
BabyStu <srw33de@gmail.com>
2023-06-27 16:57:38 -0500

    updated CSS and timer

--- script.js
+++ script.js
@@ -1,112 +1,132 @@
 const myQuestions = [
-    {
-      question: "Who invented JavaScript?",
-      answers: [
-        "Douglas Crockford",
-        "Sheryl Sandberg",
-        "Brendan Eich"
-      ],
-      correctAnswer: "Brendan Eich"
-    },
-    {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
-    }
-  ];
-  
-  const quizContainer = document.getElementById('quizContainer');
-  const startButton = document.getElementById('startButton');
-  const scoreContainer = document.getElementById('score');
+  {
+    question: "What is 2+2?",
+    answers: [
+      {text: "4", isCorrect: true},
+      {text: "5", isCorrect: false},
+      {text: "6", isCorrect: false}
+    ],
+  },
+  {
+    question: "Remainder of 23/3?",
+    answers: [
+      {text: "7", isCorrect: false},
+      {text: "4", isCorrect: false},
+      {text: "2", isCorrect: true}
+    ]
+  },
+  {
+    question: "if x = 2y-8, what does y equal?",
+    answers: [
+      {text: "x/2+4", isCorrect: true},
+      {text: "4", isCorrect: false},
+      {text: "x+4", isCorrect: false}
+    ]
+  }
+];
 
-  var currentQuestionIndex = 0;
-  
-  startButton.addEventListener('click', startQuiz);
-  
-  function startQuiz() {
-    buildQuiz();
+var currentQuestionIndex = 0;
+var score = 0;
+var timerCount = 0;
+var timer;
+
+
+var timerElement = document.getElementById('timerJS');
+
+const startButton = document.getElementById('startButton');
+const questionContainer = document.getElementById('questionContainer');
+const answersContainer = document.getElementById('answersContainer');
+const scoreContainer = document.getElementById('score');
+
+// var hiScoreSpan = document.querySelector('');
+// var namesSpan = document.querySelector('');
+// var highscore = document.getElementById('highscores')
+
+
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
+  displayQuestion();
+  timerCount = 0;
+  startTimer();
+  startButton.style.visibility = 'hidden';
+}
+
+function displayQuestion() {
+  var currentQuestion = myQuestions[currentQuestionIndex];
+
+  questionContainer.innerText = currentQuestion.question;
+  answersContainer.innerHTML = '';
+
+  for (let index = 0; index < currentQuestion.answers.length; index++) {
+    const answer = currentQuestion.answers[index];
+    const answerList = document.createElement('div');
+    const answerInput = document.createElement('input');
+    const answerLabel = document.createElement('label');
+
+    answerInput.type = 'radio';
+    answerInput.name = 'answer';
+    answerInput.value = index;
+    // answerInput.style.display = 'none';
+
+    answerLabel.textContent = answer.text;
+
+    answerList.appendChild(answerInput);
+    answerList.appendChild(answerLabel);
+    answersContainer.appendChild(answerList);
+  }
+}
+
+function grade() {
+  var percentage = ((score / myQuestions.length) * 100).toFixed(0);
+  scoreContainer.textContent = `You got ${percentage}%`;
+}
+
+function next() {
+  if (currentQuestionIndex < myQuestions.length - 1) {
+    currentQuestionIndex++;
     displayQuestion();
-    startButton.disabled = true;
+  } else {
+    questionContainer.remove();
+    answersContainer.remove();
+    startButton.remove();
+    grade();
+    stopTimer();
   }
-  
-  function displayQuestion() {
-    var questionSelector = document.getElementById('question');
-    var answerSelector = document.getElementById('answers');
-    var currentQuestion = myQuestions[currentQuestionIndex];
+}
 
-    console.log(questionSelector, answerSelector, currentQuestion)
-
-    questionSelector.innerText = currentQuestion.question;
-
-    for (let index = 0; index < currentQuestion.answers.length; index++) {
-      const answer = currentQuestion.answers[index];
-      const listItem = currentQuestion.answers;
-    
-
-      // create a list item
-      // add answer text to list item
-      // add list item to list
-
-      quizContainer.innerText = answerSelector;
-
-      console.log(answer)
-      console.log(listItem)
-    }
-
-  
-    
-
-
+function checkAnswer() {
+  const selectedAnswer = parseInt(
+    document.querySelector('input[name="answer"]:checked').value);
+  if (myQuestions[currentQuestionIndex].answers[selectedAnswer].isCorrect) {
+    score++;
   }
+  next();
+}
 
-  function buildQuiz() {
-    quizContainer.innerHTML = `
-      <h2 id ="question"></h2>
-      <ul id ="answers">
-      </ul>
-    `;
-  }
-  
+answersContainer.addEventListener('click', checkAnswer);
 
-  // function showResults() {
-   
-  //   const answerContainers = quizContainer.querySelectorAll('.answers');
-  
- 
-  //   let score = 0;
-  
-   
-  //   myQuestions.forEach((currentQuestion, questionNumber) => {
+function startTimer() {
+  timer = setInterval(function() {
+    timerCount++;
+    timerElement.textContent = timerCount;
+    console.log(timerCount)
+  }, 1000);
+} 
 
-  //     const answerContainer = answerContainers[questionNumber];
-  //     const selectedOption = answerContainer.querySelector(`input[name="question${questionNumber}"]:checked`);
-  //     const selectedAnswer = selectedOption ? selectedOption.value : '';
-  
-  
-  //     if (selectedAnswer === currentQuestion.correctAnswer) {
-  //       score++;
-     
-  //       answerContainer.style.color = 'green';
-  //     } else {
-  
-  //       answerContainer.style.color = 'red';
-  //     }
-  //   });
-  
-  //     scoreContainer.textContent = `Your score: ${score} out of ${myQuestions.length}`;
-  // }
-  
+function stopTimer() {
+  clearInterval(timer);
+}
+
+function highScores () {
+  var hiScore = localstorage.getItem('yourScore');
+  var names = localStorage.getItem('yourName');
+
+  hiScoreSpan.textContent =  yourScore;
+  namesSpan.textContent = yourName;
+}
+
+function saveScore(){
+  localStorage.setItem("percentage", JSON.stringify(percentage));
+}
+
